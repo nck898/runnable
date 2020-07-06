2fa2d37efd69217b54ade4409bc904f70ed219bb
Nick Kim <nck898@gmail.com>
2020-07-06 00:59:30 -0400

    wrapping up, but still need to make it so that it vibrates when phone screen is off

--- Runnable/app/src/main/java/com/example/runnable/MainActivity.java
+++ Runnable/app/src/main/java/com/example/runnable/MainActivity.java
@@ -1,22 +1,30 @@
 package com.example.runnable;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Build;
 import android.os.Handler;
 import android.os.SystemClock;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.os.Vibrator;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView timer ;
+    TextView timer, status ;
     Button start, pause, reset;
     long MillisecondTime, StartTime, TimeBuff, UpdateTime = 0L ;
     Handler handler;
-    int Seconds, Minutes, MilliSeconds, runCounter ;
+    int Seconds, Minutes, MilliSeconds, runCounter;
     boolean running = true;
+    Vibrator v;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +35,11 @@ public class MainActivity extends AppCompatActivity {
         start = (Button)findViewById(R.id.btStart);
         pause = (Button)findViewById(R.id.btPause);
         reset = (Button)findViewById(R.id.btReset);
+        status = (TextView)findViewById(R.id.status);
 
+        status.setText("Gotta go fast!");
         handler = new Handler() ;
+        v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 
         start.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,11 +79,13 @@ public class MainActivity extends AppCompatActivity {
                 MilliSeconds = 0 ;
 
                 timer.setText("00:00:00");
+                status.setText("Good run mate!");
 
             }
         });
 
     }
+
 
     public Runnable runnable = new Runnable() {
 
@@ -90,24 +103,27 @@ public class MainActivity extends AppCompatActivity {
 
             MilliSeconds = (int) (UpdateTime % 1000);
 
-            runCounter = Seconds/60;
             if(running) { //if you're supposed to be running
-                if (runCounter == 3) {
-                    //vibrate long once
-                    runCounter = 0;
+                if ((Seconds-3)%4 == 0) {
+
+                    v.vibrate(500);
+
                     running = false;
+                    status.setText("running");
+                    //Log.d("Run", "Run Working, seconds:" + Seconds);
+
                 }
             } else {
-                if (runCounter == 1) {
+                if (Seconds%4 == 0) {
                     //vibrate twice
-                    runCounter = 0;
+                    v.vibrate(500);
                     running = true;
+                    status.setText("walkin!");
+                    //Log.d("Rest", "Rest working, seconds:" + Seconds);
                 }
             }
 
-            timer.setText("" + Minutes + ":"
-                    + String.format("%02d", Seconds) + ":"
-                    + String.format("%03d", MilliSeconds));
+            timer.setText("" + Minutes + ":"  + String.format("%02d", Seconds) + ":" + String.format("%03d", MilliSeconds));
 
             handler.postDelayed(this, 0);
         }
